3cbc48ea2d0e6987c2dc6b7c6bb8bc596f24cba6
Edgar Méndez <mdzredgar@gmail.com>
2023-01-23 20:42:59 -0600

    Ejercicio de renderizado condicional

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [isShowText, setIsShowText] = useState(false)
+  const [isShowImg, setIsShowImg] = useState(false)
 
   const handleClick = () => {
-    setIsShowText(isShowText + 1)
+    setIsShowImg(!isShowImg)
   }
 
   return (
     <div className="App">
-      {
-        isShowText === 21 && <h1>Verdadero</h1>
-      }
-      <button onClick={handleClick}>Change</button>
+      <h1>Ejercicio</h1>
+      <p>Crear un estado que almacene un booleano que me permita mostrar una imagen cuando esta en valor true, y otra cuando este en false usando un boton que tenga la funcionalidad de cambiar el estado.</p>
+      { isShowImg ? <img src="https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg" alt="" /> : <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg" alt="" /> }
+      <button onClick={handleClick}>Click</button>
     </div>
   )
 }
